fa3158c53ee8f3f40f6850e100ad0f8faca0eb0c
lhlavati <lukahlavati@gmail.com>
2024-04-01 20:41:39 +0200

    created "hello world" junit test

---
+++ src/test/java/com/primenumbers/CalcPrimeNumTest.java
@@ -0,0 +1,11 @@
+package com.primenumbers;
+
+import org.junit.jupiter.api.Test;
+
+public class CalcPrimeNumTest {
+
+	@Test
+	public void calcPrimeNum() {
+		System.out.println("hello world");
+	}
+}
