3394374d9abdeb2d539abff29b03b53d321627e2
lhlavati <lukahlavati@gmail.com>
2024-04-02 23:32:48 +0200

    moved everything from Controller to Service

--- src/main/java/com/primenumbers/service/NumbersService.java
+++ src/main/java/com/primenumbers/service/NumbersService.java
@@ -1,12 +1,45 @@
 package com.primenumbers.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
+
+import com.primenumbers.db.Numbers;
+import com.primenumbers.repository.NumbersRepository;
 
 @Service
 public class NumbersService {
+	
+	@Autowired
+	NumbersRepository numbersRepo;
 
+	public String getPrimeNumbers(ModelMap model, int min, int max){
+		
+		Numbers numbers = numbersRepo.findByMinAndMax(min, max);
+		List<Integer> array;
+		if(numbers == null) {
+			model.put("duplicate", false);
+			array = primeNumArr(min, max);
+			String strArr = array.stream().map(Objects::toString).collect(Collectors.joining(","));
+			numbersRepo.save(new Numbers(min, max, strArr));
+		} else {
+			model.put("duplicate", true);
+			array = Arrays.stream(numbers.getPrimeNumbers().split(",")).map(Integer::parseInt).toList();
+		}
+		
+		model.put("array", array);
+		model.put("min", min);
+		model.put("max", max);
+		
+		return "index";
+	}
+	
 	public ArrayList<Integer> primeNumArr(int min, int max){
 		ArrayList<Integer> array = new ArrayList<>();
 		for (int num = min; num <= max; num++) {
@@ -17,7 +50,7 @@ public class NumbersService {
 		return array;
 	}
 	
-	public boolean isNumPrime(int num) {
+	private boolean isNumPrime(int num) {
         for (int i = 2; i <= num / 2; i++) {
             if (num % i == 0) {
                 return false;
@@ -25,5 +58,4 @@ public class NumbersService {
         }
         return true;
     }
-	
 }
--- src/main/java/com/primenumbers/web/NumberController.java
+++ src/main/java/com/primenumbers/web/NumberController.java
@@ -1,44 +1,21 @@
 package com.primenumbers.web;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.primenumbers.db.Numbers;
-import com.primenumbers.repository.NumbersRepository;
 import com.primenumbers.service.NumbersService;
 
 @Controller
 public class NumberController {
 	
 	@Autowired
-	private NumbersRepository numbersRepo;
-	
-	@Autowired
 	private NumbersService numbersService;
 	
 	@GetMapping("/numbers")
 	public String getNumbersView(ModelMap model, @RequestParam("min") int min, @RequestParam("max") int max) {
-		
-		ArrayList<Integer> array = numbersService.primeNumArr(min, max);
-		String strArr = array.toString();
-		if(numbersRepo.findByMinAndMax(min, max).isEmpty()) {
-			numbersRepo.save(new Numbers(min, max, strArr));
-			model.put("duplicate", false);
-			model.put("array", array);
-			model.put("min", min);
-			model.put("max", max);
-			return "index";
-		} else {
-			model.put("duplicate", true);
-			model.put("array", array);
-			model.put("min", min);
-			model.put("max", max);
-			return "index";
-		}
+		return numbersService.getPrimeNumbers(model, min, max);
 	}
 }
