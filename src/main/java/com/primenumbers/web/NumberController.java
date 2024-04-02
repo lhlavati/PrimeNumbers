cf4471f00a0c5428d5039dee0d29f292615aa758
lhlavati <lukahlavati@gmail.com>
2024-04-02 17:13:29 +0200

    added validation for duplicate min max values

--- src/main/java/com/primenumbers/web/NumberController.java
+++ src/main/java/com/primenumbers/web/NumberController.java
@@ -2,26 +2,47 @@ package com.primenumbers.web;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.primenumbers.db.Numbers;
+import com.primenumbers.repository.NumbersRepository;
+
 @Controller
 public class NumberController {
 	
+	@Autowired
+	private NumbersRepository numbersRepo;
+	
 	@GetMapping("/numbers")
 	public String getNumbersView(ModelMap model, @RequestParam("min") int min, @RequestParam("max") int max) {
+		
 		ArrayList<Integer> array = new ArrayList<Integer>();
 		for (int num = min; num <= max; num++) {
             if (isNumPrime(num)) {
             	array.add(num);
             }
         }
-		model.put("array", array);
-		model.put("min", min);
-		model.put("max", max);
-		return "index";
+		
+		String strArr = array.toString();
+		
+		if(numbersRepo.findByMinAndMax(min, max).isEmpty()) {
+			numbersRepo.save(new Numbers(min, max, strArr));
+			model.put("duplicate", false);
+			model.put("array", array);
+			model.put("min", min);
+			model.put("max", max);
+			return "index";
+		} else {
+			model.put("duplicate", true);
+			model.put("array", array);
+			model.put("min", min);
+			model.put("max", max);
+			return "index";
+		}
 	}
 	
 	private boolean isNumPrime(int num) {
