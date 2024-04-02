4c1e50cbef2e6e27f53193b280d331d37ed30036
lhlavati <lukahlavati@gmail.com>
2024-04-02 18:52:55 +0200

    added JUnit5 test for calculating prime numbers

--- src/test/java/com/primenumbers/PrimeNumbersApplicationTests.java
+++ src/test/java/com/primenumbers/PrimeNumbersApplicationTests.java
@@ -1,13 +1,44 @@
 package com.primenumbers;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.primenumbers.service.NumbersService;
+
 @SpringBootTest
-class PrimeNumbersApplicationTests {
+public class PrimeNumbersApplicationTests {
+	
+	@Autowired
+	private NumbersService numbersService;
 
 	@Test
-	void contextLoads() {
+	public void calcPrimeNum() {
+		ArrayList<Integer> arrExpected = new ArrayList<>();
+		arrExpected.add(5);
+		arrExpected.add(7);
+		arrExpected.add(11);
+		arrExpected.add(13);
+		arrExpected.add(17);
+		arrExpected.add(19);
+		ArrayList<Integer> arrActual = numbersService.primeNumArr(5, 20);
+		
+		int[] arrayExpected = arrExpected.stream().mapToInt(i -> i).toArray();
+		int[] arrayActual = arrActual.stream().mapToInt(i -> i).toArray();
+		
+		System.out.println(arrayExpected);
+		System.out.println(arrayActual);
+		assertArrayEquals(arrayExpected, arrayActual);
+		
+		/*
+		 * if(arrExpected.equals(arrActual)) { System.out.println("Same");
+		 * System.out.println(arrExpected); System.out.println(arrActual); } else {
+		 * System.out.println("Not same"); System.out.println(arrExpected);
+		 * System.out.println(arrActual); }
+		 */
 	}
-
 }
