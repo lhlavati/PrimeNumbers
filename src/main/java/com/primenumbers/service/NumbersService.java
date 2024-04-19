2f1e95efd4fd9fa9c67e46c77bf2f1ea812602db
lhlavati <lukahlavati@gmail.com>
2024-04-19 12:23:27 +0200

    slight adjustments in isNumPrime method

--- src/main/java/com/primenumbers/service/NumbersService.java
+++ src/main/java/com/primenumbers/service/NumbersService.java
@@ -54,7 +54,7 @@ public class NumbersService {
 	}
 	
 	private boolean isNumPrime(int num) {
-        for (int i = 2; i <= num / 2; i++) {
+        for (int i = 2; i < num; i++) {
             if (num % i == 0) {
                 return false;
             }
